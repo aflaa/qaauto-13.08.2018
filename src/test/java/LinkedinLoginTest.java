1141022361d9309c7ac89c61a8d627907d79269d
aflaa <myalname@gmail.com>
2018-08-20 12:51:53 +0300

    fixed getText by getAttribute

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class LinkedinLoginTest {
 
     @Test
-    public void succeddfulLoginTest () {
+    public void succeedfulLoginTest () {
         //navigate to linkedin.com
         //verify that login page is loaded
         //enter userEmail
@@ -34,8 +34,10 @@ public class LinkedinLoginTest {
         Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up","Login page title is wrong.");
 
         WebElement signButton=driver.findElement(By.xpath(signInButton_xpath));
-        Assert.assertEquals(signButton.getText(), "Sign in","Sign in button is unavailable");
+        Assert.assertEquals(signButton.getAttribute("value"), "Sign in","Sign in button is unavailable");
+        //Assert.assertEquals(signButton.getText(), "Sign in","Sign in button is unavailable");
        // Assert.assertNotNull(driver.findElement(By.xpath("//input[@class='login submit-button']").getText()));
+
 
         WebElement loginEmail=driver.findElement(By.xpath(loginField_xpath));
         loginEmail.clear();
@@ -48,7 +50,7 @@ public class LinkedinLoginTest {
         signButton.click();
 
         WebElement profilePhoto=driver.findElement(By.xpath(profilePhoto_xpath));
-        Assert.assertEquals(profilePhoto.getText(), userName,"User name is incorrect");
+        Assert.assertEquals(profilePhoto.getAttribute("alt"), userName,"User name is incorrect");
         profilePhoto.click();
 
         driver.findElement(By.xpath(signOutButton_xpath)).click();
